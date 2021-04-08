8d538c5315a33ef64636466f4db14fd3342af646
becode <ingrid.feijt@hotmail.com>
2021-04-08 23:51:10 +0200

    update

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -1,7 +1,39 @@
+// data
+const products = [
+    { id: 1, description: "Quarz Luxe", price: 12, img: 'assets/img/quarz-luxe.JPG'},
+    { id: 2, description: 'Curren Business', price: 20, img: 'assets/img/curren-business.JPG'},
+    { id: 3, description: 'Curren Sport', price: 5, img: 'assets/img/curren-sport.JPG'},
+    { id: 4, description: 'Jaragar Racing', price: 8, img: 'assets/img/jaragar-racing.JPG'},
+    { id: 5, description: 'Liges Hommes', price: 3, img: 'assets/img/liges-hommes.JPG'},
+    { id: 6, description: 'Maserati Mechanical', price: 65, img: 'assets/img/maserati-mechanical.JPG'},
+    { id: 7, description: 'Montre Mecanique', price: 25, img: 'assets/img/montre-mecanique.JPG'},
+    { id: 8, description: 'Brand Designer', price: 28, img: 'assets/img/brand-designer.JPG'},
+    { id: 9, description: 'Relogio Masculino', price: 4, img: 'assets/img/relogio-masculino.JPG'},
+    { id: 10, description: 'Tissot Multifunction', price: 29, img: 'assets/img/tissot-multifunction.JPG'},
+    { id: 11, description: 'Hip Hop Gold', price: 87, img: 'assets/img/hiphop-gold.JPG'},
+    { id: 12, description: 'Mesh Genova', price: 6, img: 'assets/img/mesh-genova.JPG'},
+  ];
+
 // Components: structure du site
 const Home = {
-    template: '<h1>Home</h1>',
-    name: 'Home'
+    template: '#home',
+    name: 'Home',
+    data: () => {
+        return {
+            products,
+            searchKey: '',
+        }
+    },
+    computed: {
+        filteredList(){
+            return this.products.filter((product)=> {
+                return product.description.toLowerCase().includes(this.searchKey.toLowerCase());
+            })
+        }
+    },
+    // methods: {
+
+    // },
 }
 const UserSettings = {
     template: '<h1>UserSettings</h1>',
@@ -17,9 +49,9 @@ const ShoppingCart = {
 }
 
 // Routeur
-const router = new VueRouteur({
+const router = new VueRouter({
     routes: [
-        {path:'/', component: 'Home', name: 'Home'},
+        {path:'/', component: Home, name: 'Home'},
         {path:'/user-settings', component: UserSettings, name: 'UserSettings'},
         {path:'/wish-list', component: WishList, name: 'WishList'},
         {path:'/shopping-cart', component: ShoppingCart, name: 'ShoppingCart'},
@@ -27,6 +59,6 @@ const router = new VueRouteur({
 })
 
 // Pointe vers app dans le html
-const vue = new vue({
+const vue = new Vue({
     router
-}).mount('#app');
+}).$mount('#app');
